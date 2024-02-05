18d1e28ebf17fbe41656fe9a350f7dca8e02f36c
Ba <djeinaba.ba@accenture.com>
2024-02-05 21:56:31 +0100

    first commit

---
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -0,0 +1,71 @@
+package com.globomatics.bike.models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import javax.persistence.Entity;
+@Entity
+public class Bike {
+    private String name;
+    private String email;
+    private String phone;
+    private String model;
+    private BigDecimal serialNumber;
+    private Date purchaseDate;
+    private boolean contact;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public BigDecimal getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(BigDecimal serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public boolean isContact() {
+        return contact;
+    }
+
+    public void setContact(boolean contact) {
+        this.contact = contact;
+    }
+}
